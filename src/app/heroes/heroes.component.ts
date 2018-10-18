09b5d0837e41735989fe1c30b29d8f94a6c268fc
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-10-17 22:37:29 -0300

    Concluída a navegação, sendo que os detalhes de cada herói agora ficaram em uma página separada.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,19 +9,17 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+
+  private getHeroes() {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
